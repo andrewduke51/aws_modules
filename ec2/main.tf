772bb790c14ae852065858ddc308ca1360636581
AndrewF <andrew@sytrue.com>
2022-11-11 18:05:15 -0500

    If empty

--- ec2/main.tf
+++ ec2/main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "ubuntu" {
 ####### EC2 ############
 resource "aws_instance" "ec2" {
   count                       = var.ec2
-  ami                         = var.ami_id != "" ? var.ami_id : data.aws_ami.ubuntu.id
+  ami                         = var.ami_id == "" ? data.aws_ami.ubuntu.id : var.ami_id
   instance_type               = var.instance_type
   key_name                    = "ansible"
   associate_public_ip_address = true
