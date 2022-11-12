bf6b03497030688cdefedfa8f6f299c3bb0e68a7
AndrewF <andrew@sytrue.com>
2022-11-11 22:05:29 -0500

    Use vpc_security_group_ids

--- ec2/main.tf
+++ ec2/main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "ec2" {
   associate_public_ip_address = true
   user_data                   = var.user_data != "" ? var.user_data : base64encode(file("${path.module}/templates/user-data.sh"))
   subnet_id                   = var.subnet_id
-  security_groups             = var.security_groups
+  vpc_security_group_ids      = var.security_groups
 
   tags = {
     Name = var.tag_name
